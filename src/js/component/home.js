92a14b3dcc39ed014ba245d59394579625aeee5c
Paulo Jiménez <pauloajimenezm76@outlook.com>
2021-05-27 04:58:59 +0000

    code done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "bootstrap";
+import { unmountComponentAtNode } from "react-dom/cjs/react-dom.production.min";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [luz1, setLuz1] = useState("apagado");
+	const [luz2, setLuz2] = useState("apagado");
+	const [luz3, setLuz3] = useState("apagado");
+
+	const handleClick = e => {
+		let seleccion = e.target.id;
+
+		if (seleccion === "red") {
+			if (luz1 === "red") {
+				setLuz1("apagado");
+			} else {
+				setLuz1("red");
+				setLuz2("apagado");
+				setLuz3("apagado");
+			}
+		}
+		if (seleccion === "yellow") {
+			if (luz2 === "yellow") {
+				setLuz2("apagado");
+			} else {
+				setLuz2("yellow");
+				setLuz1("apagado");
+				setLuz3("apagado");
+			}
+		}
+
+		if (seleccion === "green") {
+			if (luz3 === "green") {
+				setLuz3("apagado");
+			} else {
+				setLuz3("green");
+				setLuz1("apagado");
+				setLuz2("apagado");
+			}
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			{}
+			<div className="center">
+				<div
+					id="red"
+					className={luz1}
+					onClick={event => handleClick(event)}></div>
+				<div
+					id="yellow"
+					className={luz2}
+					onClick={event => handleClick(event)}></div>
+				<div
+					id="green"
+					className={luz3}
+					onClick={event => handleClick(event)}></div>
+			</div>
 		</div>
 	);
 }
